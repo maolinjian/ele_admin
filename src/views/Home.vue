db8fa819a23feec03ac9a2d47bd8cac44c4a68e6
maolinjian <maolinjian@163.com>
2021-11-15 16:45:43 +0800

    docs(初始化项目环境): 增加esLint pretter git cz 的约束

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+// 写代码的时候,保存的时候自动将代码格式化(eslint 规范)
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('21eu98')
   }
 }
 </script>
